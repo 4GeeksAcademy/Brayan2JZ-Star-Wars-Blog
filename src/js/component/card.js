14fdd53061839b28865145ecade38b9f0e9d9a11
Brayan2JZ <b.sredzinski92@gmail.com>
2024-09-18 22:03:21 +0000

    Commit

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -5,27 +5,27 @@ import { Context } from "../store/appContext";
 
 import "../../styles/demo.css";
 
-export const Card = ({item, index, category}) => {
-	const {store, actions} = useContext(Context);
-	const GUIDE_URL="https://starwars-visualguide.com/assets/img"
+export const Card = ({ item, index, category }) => {
+	const { store, actions } = useContext(Context);
+	const GUIDE_URL = "https://starwars-visualguide.com/assets/img"
 	return (
-		// <div class="d-flex flex-row flex-nowrap overflow-auto">
-			<div className="card" style={{ width: "18rem" }}>
-				<img src={`${GUIDE_URL}/${category}/${index+1}.jpg`}
-					className="card-img-top" alt="..." />
-				<div className="card-body">
+		<div className="card" style={{ minWidth: "18rem" }}>
+			<img src={`${GUIDE_URL}/${category}/${index + 1}.jpg`}
+				className="card-img-top" alt="..." />
+			<div className="card-body d-flex flex-column">
 				<h5 className="card-title">{item.name}</h5>
 				<p className="card-text">
 					{
-						category=="characters" ? "Gender: "+ item.gender :
-						category=="planets" ? "Population: "+ item.population:
-						"Crew: " + item.crew
+						category == "characters" ? "Gender: " + item.gender :
+							category == "planets" ? "Population: " + item.population :
+								"Crew: " + item.crew
 					}
 				</p>
-				<button type="button" class="btn btn-outline-primary" disabled>Primary button</button>
-				<button type="button" class="btn btn-outline-warning" disabled>♥</button>
+				<div className="buttons-div mt-auto d-flex justify-content-between">
+					<button type="button" class="btn btn-outline-primary" disabled>Learn More!</button>
+					<button type="button" class="btn btn-outline-warning" disabled>♥</button>
 				</div>
 			</div>
-		// </div>
+		</div>
 	);
-  };
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -9,36 +9,32 @@ export const Home = () => {
 
 	return (
 
-		<div className="body">
-			<h2 className="characters ms-4">Characters</h2>
-			<div className="d-flex flex-column w-100 mt-3 align-items-center">
-				<div class="d-flex flex-row flex-nowrap overflow-scroll align-items-stretch">
+		<div className="body ms-2">
+			<h2>Characters</h2>
+			<div className="card-div d-flex flex-row align-items-center flex-nowrap overflow-scroll align-items-stretch">
 					{store.characters.map((item, index) => {
 						return (
 							<Card item={item} index={index} key={index} category="characters" />
 						)
 					})}
-				</div>
 			</div>
-			<div className="d-flex flex-column w-100 mt-3 align-items-center">
-				<h1>Planets</h1>
-				<div class="d-flex flex-row flex-nowrap overflow-scroll align-items-stretch">
+
+			<h2>Planets</h2>
+			<div className="card-div d-flex flex-row align-items-center flex-nowrap overflow-scroll align-items-stretch">
 					{store.planets.map((item, index) => {
 						return (
 							<Card item={item} index={index} key={index} category="planets" />
 						)
 					})}
-				</div>
 			</div>
-			<div className="d-flex flex-column w-100 mt-3 align-items-center">
-				<h1>Vehicles</h1>
-				<div class="d-flex flex-row flex-nowrap overflow-scroll align-items-stretch">
+
+			<h2>Vehicles</h2>
+			<div className="card-div d-flex flex-row align-items-center flex-nowrap overflow-scroll align-items-stretch">
 					{store.vehicles.map((item, index) => {
 						return (
 							<Card item={item} index={index} key={index} category="vehicles" />
 						)
 					})}
-				</div>
 			</div>
 		</div>
 	);
